113f3120f78ad2785900d1807577dd7236a95bc2
EvelinaAB <eweburdzel@gmail.com>
2021-12-19 07:26:21 +0100

    Caballos Sockets Servidor+Cliente

--- CarreraCaballosSockets/src/carreracaballossocketsServidor/Caballo.java
+++ CarreraCaballosSockets/src/carreracaballossocketsServidor/Caballo.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package carreracaballossocketsServidor;
 
-/**
- *
- * @author ewelina
- */
-public class Caballo {
-    
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Caballo implements Runnable, Comparable<Caballo> {
+
+    Reloj reloj = new Reloj();
+    private String nombre;
+    private int contador;
+    private long tiempoRecorrido;
+
+    public Caballo(String nombre, long tiempoR) {
+        this.nombre = nombre;
+        this.tiempoRecorrido = tiempoR;
+    }
+
+    @Override
+
+    public void run() {
+
+        Correr();
+
+    }
+
+    private synchronized void Correr() {
+        reloj.start();
+        while (contador < 100) {
+
+            try {
+                Thread.sleep((int) (Math.random() * 41 + 10));
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Caballo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            contador++;
+
+        }
+        reloj.stop();
+        this.tiempoRecorrido = reloj.tiempoTranscurrido();
+
+        System.out.println("Soy " + this.nombre + "  he terminado y he tardado  " + this.tiempoRecorrido + " ms");
+
+    }
+
+    @Override
+    public String toString() {
+
+        return nombre + " " + tiempoRecorrido + " ms";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public long getTiempoRecorrido() {
+        return tiempoRecorrido;
+    }
+
+    @Override
+    public int compareTo(Caballo o) {
+        if (this.tiempoRecorrido > o.tiempoRecorrido) {
+            return 1;
+        }
+        if (this.tiempoRecorrido < o.tiempoRecorrido) {
+            return -1;
+
+        }
+        return 0;
+    }
+
 }
--- CarreraCaballosSockets/src/carreracaballossocketsServidor/Servidor.java
+++ CarreraCaballosSockets/src/carreracaballossocketsServidor/Servidor.java
@@ -1,9 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package carreracaballossocketsServidor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
 
 /**
  *
@@ -11,11 +14,67 @@ package carreracaballossocketsServidor;
  */
 public class Servidor {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        final int puerto = 3030;
+
+        try {
+
+            ServerSocket socket = new ServerSocket(puerto);
+
+            System.out.println("Servidor listo...");
+            while (true) {
+                Socket socketCliente = socket.accept();
+                atender(socketCliente);
+                socketCliente.close();
+            }
+        } catch (IOException ex) {
+
+        }
     }
-    
+
+    private static void atender(Socket socketCliente) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+            PrintWriter pw = new PrintWriter(socketCliente.getOutputStream());
+
+            int length;
+            long tiempo = 0;
+
+            length = br.read();
+            /*Creamos array de caballos*/
+            Caballo[] caballos = new Caballo[length];
+            /*Recorremos la longitud del array y le asignamos los caballos*/
+            for (int i = 0;
+                    i < length;
+                    i++) {
+                Caballo c = new Caballo("Caballo " + i, tiempo);
+                caballos[i] = c;
+            }
+            Thread[] threads = new Thread[length];
+            for (int i = 0;
+                    i < length;
+                    i++) {
+                Thread thread = new Thread(caballos[i]);
+                threads[i] = thread;
+
+                thread.start();
+
+            }
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Arrays.sort(caballos);
+            for (Caballo c : caballos) {
+                pw.print(c);
+
+            }
+        } catch (IOException ex) {
+
+        }
+
+    }
 }
+
