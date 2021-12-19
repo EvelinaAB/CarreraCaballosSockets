127c7c0ed9ce6d7b662ecd170e186a770f6c9b64
EvelinaAB <eweburdzel@gmail.com>
2021-12-19 07:22:53 +0100

    Clase Reloj

--- CarreraCaballosSockets/src/carreracaballossocketsServidor/Reloj.java
+++ CarreraCaballosSockets/src/carreracaballossocketsServidor/Reloj.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package carreracaballossocketsServidor;
 
 /**
@@ -10,5 +6,33 @@ package carreracaballossocketsServidor;
  * @author ewelina
  */
 public class Reloj {
-    
+
+    private final long nanoSegundosPorMilisegundos = 1000000;
+
+    private long relojInicio = 0;
+    private long relojStop = 0;
+    private boolean relojEnCurso = false;
+
+    public void start() {
+        this.relojInicio = System.nanoTime();
+        this.relojEnCurso = true;
+    }
+
+    public void stop() {
+        this.relojStop = System.nanoTime();
+        this.relojEnCurso = false;
+    }
+
+    public long tiempoTranscurrido() {
+        long transcurrido;
+
+        if (relojEnCurso) {
+            transcurrido = (System.nanoTime() - relojInicio);
+        } else {
+            transcurrido = (relojStop - relojInicio);
+        }
+
+        return transcurrido / nanoSegundosPorMilisegundos;
+    }
+
 }
